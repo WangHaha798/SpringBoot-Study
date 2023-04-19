3cea72b3adb4debfb0e9f44716ef769440210f11
wzh <1711688082@qq.com>
2023-04-19 16:03:20 +0800

    真实 创建项目

--- SpringBoot-study/src/main/java/com/study/SpringBootStudyApplication.java
+++ SpringBoot-study/src/main/java/com/study/SpringBootStudyApplication.java
@@ -8,7 +8,5 @@ public class SpringBootStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStudyApplication.class, args);
-		System.out.println("sss");
 	}
-
 }
