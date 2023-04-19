db2d4908ffa50bb5cac7965d31224290ba777573
wzh <1711688082@qq.com>
2023-04-19 15:39:00 +0800

    创建项目2

--- SpringBoot-study/src/main/java/com/study/SpringBootStudyApplication.java
+++ SpringBoot-study/src/main/java/com/study/SpringBootStudyApplication.java
@@ -8,6 +8,7 @@ public class SpringBootStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStudyApplication.class, args);
+		System.out.println("test");
 	}
 
 }
